b5517350dcc2660b971bc9b72a688579879533ce
peterstanton <stantp2@uw.edu>
2017-11-21 16:27:43 -0800

    randomly hit wrong links for errors.

--- trafficgen.py
+++ trafficgen.py
@@ -1,17 +1,7 @@
-import argparse
-import datetime
-#import httplib
 import sys
 import time
-import threading
-import os
 import subprocess
 import random
-import numpy
-
-
-import trafficstats
-
 
 url = sys.argv[1]
 rps = int(sys.argv[2])
@@ -20,14 +10,23 @@ jitter = float(sys.argv[3])
 lower = rps * (1.0 - jitter)
 upper = rps * (1.0 + jitter)
 
-actual = random.randint(lower,upper)
+actual = random.randint(lower, upper)
 
 invoke = "curl -G " + url
 
 invoke = invoke.split(" ")
 
 while True:
-	start = time.time()
-	interval = 1
-	for i in range(actual):
-		process = subprocess.call(invoke)
+    start = time.time()
+    interval = 1
+    for i in range(actual):
+        chance = random.randint(0, 100)
+        if chance in range(0, 5):
+            invoke += 'arglebargle'
+            process = subprocess.call(invoke)
+        elif chance in range(6, 10):
+            invoke += '/fail'
+            process = subprocess.call(invoke)
+        else:
+            process = subprocess.call(invoke)
+
