49ef5819b7ebbdb8ca6b6c4d5bd89fbd0d6abe53
Peter Stanton <peterstanton@users.noreply.github.com>
2017-11-26 14:42:27 -0800

    Add files via upload
    tweaked traffic generator.

--- trafficgen.py
+++ trafficgen.py
@@ -17,18 +17,26 @@ invoke = "curl -G " + url
 
 invoke = invoke.split(" ")
 
+permGood = invoke[2]
+
 while True:
     start = time.time()
     interval = 1
     for i in range(actual):
+	invoke[2] = permGood
         chance = random.randint(0, 100)
-        if chance in range(0, 4):
-            invoke += 'arglebargle'
+	print("Result is: " + str(chance))
+        if chance in range(0, 5):
+            #invoke[2] = invoke[2][:12] + "arglebargle" + invoke[2][12:]
+	    invoke[2]  = invoke[2] + '/arglebargle'
+            print("Bad URL: " + str(invoke))
             process = subprocess.call(invoke)
-        elif chance in range(5, 10):
-            invoke += '/fail'
+        elif chance in range(6, 10):
+            invoke[2] += "/fail"
+	    print("Server fail: " + str(invoke))
             process = subprocess.call(invoke)
         else:
+	    print("Correct: " + str(invoke))
             process = subprocess.call(invoke)
     end = time.time()
     sleep(1-(start-end))
